379cf7f11e443c94af61dc03b083345ff828ca39
Viczián Dániel <113669347+Vicziand@users.noreply.github.com>
2024-11-15 14:08:29 +0100

    Update home.py

--- src/main/home.py
+++ src/main/home.py
@@ -6,6 +6,7 @@ with col1:
     st.title("Health Monitoring")
 with col2:
     st.image("src/health_monitoring.jpg", width=400)
+st.write("/n")
 
 st.markdown("""Az alkalmazás célja az egészségügyi adatok folyamatos figyelése és elemzése. A felületen két predikció érhető el:
 - Szívkoszorúér-betegség kockázatának előrejelzése
@@ -15,8 +16,8 @@ col3, col4 = st.columns(2, gap="small")
 with col3:  
     st.markdown("""Az alkalmazás rendelkezik egy bejelentkezés funkcióval, amelyen keresztül a Garminconnect fiókhoz kapcsolódva lehet importálni az aktuális adatokat, amelyek segítségével elvégezhető a predikció.
      A bejelentkezéshez a GarminConnect alkalmazás regisztrációjakor használt felhasználónév és jelszó szükséges""")
-
 with col4:
     st.image("src/smartwatch_photo.jpg", width=400)
+st.write("/n")
 
 st.markdown("""Az előrejelzések nem minősülnek valódi orvosi diagnózisnak. Az eredmények kizárólag tájékoztatási célt szolgálnak, és nem helyettesítik az orvosi konzultációt vagy szakértelmet.""")
