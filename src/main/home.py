462ac2df6cd65343fcaca9caa79289c651ed3f54
Viczián Dániel <113669347+Vicziand@users.noreply.github.com>
2024-11-15 12:21:03 +0100

    Update home.py

--- src/main/home.py
+++ src/main/home.py
@@ -1,13 +1,22 @@
 import streamlit as st
 
-st.title("Health Monitoring")
-st.image("src/health_monitoring.jpg", width=400)
 
-st.markdown("""Az alkalmazás célja az egészségügyi adatok folyamatos figyelése és elemzése. A felületen két predikció érhető el: szívkoszorúér-betegség kockázatának előrejelzésére, valamint az alvási rendellenességek felismerésére.
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.title("Health Monitoring")
+with col2:
+    st.image("src/health_monitoring.jpg", width=400)
 
-Az alkalmazás rendelkezik egy bejelentkezés funkcióval, amelyen keresztül a Garminconnect fiókhoz kapcsolódva lehet importálni az aktuális adatokat, amelyek segítségével elvégezhető a predikció.
-Bejelentkezés után egy extra funkció is elérhetővé válik, amely a frissen betöltött adatok grafikus megjelenítését biztosítja, hogy könnyedén nyomon követhesd a eredményidet. A bejelentkezés hez a GarminConnect alkalmazás regisztrációjakor használt felhasználónév és jelszó szükséges
+st.markdown("""Az alkalmazás célja az egészségügyi adatok folyamatos figyelése és elemzése. A felületen két predikció érhető el:
+- Szívkoszorúér-betegség kockázatának előrejelzése
+- Alvási rendellenességek felismerésére""")
 
-Az előrejelzések nem minősülnek valódi orvosi diagnózisnak. Az eredmények kizárólag tájékoztatási célt szolgálnak, és nem helyettesítik az orvosi konzultációt vagy szakértelmet.""")
+col3, col4 = st.columns(2, gap="small")
+with col3:  
+    st.markdown("""Az alkalmazás rendelkezik egy bejelentkezés funkcióval, amelyen keresztül a Garminconnect fiókhoz kapcsolódva lehet importálni az aktuális adatokat, amelyek segítségével elvégezhető a predikció.
+     A bejelentkezéshez a GarminConnect alkalmazás regisztrációjakor használt felhasználónév és jelszó szükséges""")
 
+with col4:
+    st.image("src/smartwatch_photo.jpg", width=400)
 
+st.markdown("""Az előrejelzések nem minősülnek valódi orvosi diagnózisnak. Az eredmények kizárólag tájékoztatási célt szolgálnak, és nem helyettesítik az orvosi konzultációt vagy szakértelmet.""")
